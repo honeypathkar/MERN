a0e520e54cc34c16cf3d02a753d12749c70e998f
honeypatkar <honeypatkar70@gmail.com>
2024-05-17 15:01:54 +0530

    Path modules

---
+++ Path/index.js
@@ -0,0 +1,6 @@
+const path = require("path");
+
+console.log(path.extname("/home/honeypatkar70/NodeJs/Path/index.js"));
+console.log(path.basename("/home/honeypatkar70/NodeJs/Path/index.js"));
+console.log(path.parse("/home/honeypatkar70/NodeJs/Path/index.js").dir)
+
