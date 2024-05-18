9c47e42f80c57d7cb30823767441393d30a2a088
honeypatkar <honeypatkar70@gmail.com>
2024-05-18 14:53:01 +0530

    Custom Modules

---
+++ ImportExport/index.js
@@ -0,0 +1,4 @@
+const oper = require("./oper");
+
+console.log(oper.add(4, 2));
+console.log(oper.name)
---
+++ ImportExport/oper.js
@@ -0,0 +1,7 @@
+const add = (a, b) => {
+    return a + b;
+}
+
+const name = "Honey"
+
+module.exports = {add, name}
