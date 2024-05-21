11aec138a9e3546f83c091c264f91acee957ceb1
honeypatkar <honeypatkar70@gmail.com>
2024-05-21 14:15:34 +0530

    Handling HTTP Request

--- httpServer/index.js
+++ httpServer/index.js
@@ -1,6 +1,15 @@
 const http = require("http");
 const server = http.createServer((req, res) => {
-  res.end("Hello ! How are you ?");
+  if (req.url == "/") {
+    res.end("Home Page");
+  } else if (req.url == "/about") {
+    res.end("About Page");
+  } else if (req.url == "/contact") {
+    res.end("Contact Page");
+  } else {
+    res.writeHead(404, { "Content-type": "text/html" });
+    res.end("<h1>404 Page Not Found</h1>");
+  }
 });
 
 server.listen(8000, "localhost", () => {
