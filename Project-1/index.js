b88576167bd190e6e93fdedcaeaf2c60259d75fb
honeypatkar <honeypatkar70@gmail.com>
2024-05-29 14:56:32 +0530

    Getting data from user and store it in files

--- Project-1/index.js
+++ Project-1/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const path = require("path");
+const fs = require("fs");
 
 const app = express();
 
@@ -9,16 +10,28 @@ app.use(express.static(path.join(__dirname, "public")));
 app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
-  res.render("index");
+  fs.readdir(`./files`, (err, files) => {
+    res.render("index", { files: files });
+  });
 });
 
-app.get("/profile/:username", (req, res) => {
-  res.send(`Welcome ${req.params.username}`);
+app.post("/create", (req, res) => {
+  fs.writeFile(
+    `./files/${req.body.title.split(' ').join('')}.txt`,
+    req.body.detail,
+    (err) => {
+      res.redirect("/");
+    }
+  );
 });
 
-app.get("/author/:author/:age", (req, res) => {
-  res.send(`Author Name : ${req.params.author} and Age ${req.params.age}`);
-});
+// app.get("/profile/:username", (req, res) => {
+//   res.send(`Welcome ${req.params.username}`);
+// });
+
+// app.get("/author/:author/:age", (req, res) => {
+//   res.send(`Author Name : ${req.params.author} and Age ${req.params.age}`);
+// });
 
 app.listen(3000, () => {
   console.log("Listening to port no. 3000");
