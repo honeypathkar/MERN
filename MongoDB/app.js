165d9da144d9272e19d3eb6876490df8ac739c66
honeypathkar <honeypatkar70@gmail.com>
2024-11-09 12:52:20 +0530

    Curd Operation using ejs

--- MongoDB/app.js
+++ MongoDB/app.js
@@ -1,45 +1,51 @@
-//CRUD Operation
-
 const express = require("express");
 const app = express();
+const path = require("path");
+const userModel = require("./models/user");
 
-const userModel = require("./usermodel");
+app.set("view engine", "ejs");
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
-  res.send("Hello");
-});
-
-app.get("/create", async (req, res) => {
-  let createdUser = await userModel.create({
-    name: "Harsh Pathkar",
-    userName: "harshpathkar",
-    email: "harsh@gmail.com",
-  });
-
-  res.send(createdUser);
+  res.render("index");
 });
 
 app.get("/read", async (req, res) => {
   let users = await userModel.find();
-  res.send(users);
+  res.render("read", { users });
 });
 
-app.get("/update", async (req, res) => {
-  let updatedUser = await userModel.findOneAndUpdate(
-    { userName: "honeypathkar" },
-    { userName: "honeypathkar70" },
+app.get("/edit/:userId", async (req, res) => {
+  let user = await userModel.findOne({ _id: req.params.userId });
+  res.render("edit", { user });
+});
+
+app.post("/update/:userId", async (req, res) => {
+  let { name, email, image } = req.body;
+  let user = await userModel.findOneAndUpdate(
+    { _id: req.params.userId },
+    { name, email, image },
     { new: true }
   );
-
-  res.send(updatedUser);
+  res.redirect("/read");
 });
 
-app.get("/delete", async (req, res) => {
-  let deletedUser = await userModel.findOneAndDelete({
-    userName: "honeypathkar70",
+app.get("/delete/:id", async (req, res) => {
+  let users = await userModel.findOneAndDelete({ _id: req.params.id });
+  res.redirect("/read");
+});
+
+app.post("/create", async (req, res) => {
+  let { name, email, image } = req.body;
+  let createdUser = await userModel.create({
+    name,
+    email,
+    image,
   });
 
-  res.send(deletedUser);
+  res.redirect("/read");
 });
 
 app.listen(3000);
--- MongoDB/models/user.js
+++ MongoDB/models/user.js
@@ -1,11 +1,11 @@
 const mongoose = require("mongoose");
 
-mongoose.connect(`mongodb://127.0.0.1:27017/mongodbpractice`);
+mongoose.connect("mongodb://127.0.0.1:27017/userdata");
 
 const userSchema = mongoose.Schema({
-  name: "String",
-  userName: "String",
-  email: "String",
+  image: String,
+  email: String,
+  name: String,
 });
 
 module.exports = mongoose.model("user", userSchema);
