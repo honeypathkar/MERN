e4509bf139e392d5bfafd6dc10e3e30487184622
honeypathkar <honeypatkar70@gmail.com>
2024-12-21 14:44:22 +0530

    Creating Routes

--- Scatch/app.js
+++ Scatch/app.js
@@ -6,15 +6,19 @@ const db = require("./config/mongoose-connection");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 
+const ownersRouter = require("./routes/ownersRouter");
+const usersRouter = require("./routes/usersRouter");
+const productsRouter = require("./routes/productsRouter");
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 app.set("view engine", "ejs");
 
-app.get("/", (req, res) => {
-  res.send("Hello World");
-});
+app.use("/owners", ownersRouter);
+app.use("/users", usersRouter);
+app.use("/products", productsRouter);
 
 app.listen(3000, () => {
   console.log("Listen to port 3000");
